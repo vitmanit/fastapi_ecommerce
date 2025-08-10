dc30aef9f3ec4752bac6ca57dbaf9eb7341daac5
Андрей <manskii.2015@mail.ru>
2025-08-11 01:33:21 +0300

    Логирование

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
 from app.routers import category, products, auth, permission, review
+from loguru import logger
+from uuid import uuid4
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 @app.get("/")
 async def welcome() -> dict:
